4107f217c0a116d708740063410210b64757c106
Tim De Pauw <timdp@users.noreply.github.com>
2017-11-17 19:18:43 +0100

    karma-mocha-iframes@0.5.0: suppress hooks in outer iframe

--- test/dummy.spec.js
+++ test/dummy.spec.js
@@ -1,6 +1,26 @@
 var expect = window.chai.expect
 
+var isInInnerIframe = function () {
+  return (window.location.href.indexOf('/iframe.html') >= 0)
+}
+
+beforeEach(function () {
+  expect(isInInnerIframe()).to.be.true()
+})
+
+after(function () {
+  expect(isInInnerIframe()).to.be.true()
+})
+
 describe('suite', function () {
+  before(function () {
+    expect(isInInnerIframe()).to.be.true()
+  })
+
+  afterEach(function () {
+    expect(isInInnerIframe()).to.be.true()
+  })
+
   it('succeeds', function () {
     expect(true).to.be.true()
   })
